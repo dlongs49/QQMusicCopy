88336e4756d43c591592a5adce5d0d44e2cbc691
dinglong <dillonl49@163.com>
2024-03-25 23:21:35 +0800

    feat:#增加settMenu类文件

---
+++ src/lib/popup/settMenu.h
@@ -0,0 +1,93 @@
+//
+// Created by dinglong on 2024/3/25 下午11:20
+//
+
+#ifndef QQMUSICCOPY_SETTMENU_H
+#define QQMUSICCOPY_SETTMENU_H
+
+#include <QWidget>
+#include <QFrame>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <QFile>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QEventLoop>
+#include <QByteArray>
+#include <QPainter>
+#include <QPainterPath>
+#include <QRect>
+#include <QGraphicsDropShadowEffect>
+#include <QPushButton>
+#include <QPixmap>
+#include <QDebug>
+#include <QEvent>
+#include <QProxyStyle >
+
+class SettMenu : public QWidget {
+Q_OBJECT;
+public:
+    SettMenu(QWidget *parent = nullptr);
+
+    bool eventFilter(QObject *obj, QEvent *event);
+
+signals:
+public slots:
+
+    void handleClear();
+
+    void loadQSS();
+
+protected:
+
+private:
+    QFrame *frame;
+    QPushButton *clearBtn;
+    QGraphicsDropShadowEffect *shadow;
+    QLabel *hotLabel;
+    QLabel *txtLabel[10];
+    QLabel *hisTxtLabel[10];
+    QLabel *numLabel;
+    QLabel *titLabel;
+    QLabel *hisbox;
+    QLabel *histTit;
+    QLabel *noHisLabel;
+    QLabel *noHisTxt;
+    QLabel *downLabel;
+    QLabel *skinLabel;
+    QLabel *settingLabel;
+    QLabel *recoverLabel;
+    QLabel *minimizeLabel;
+    QLabel *maximizeLabel;
+    QLabel *closeLabel;
+    QWidget *widget;
+    QWidget *lWidget;
+    QWidget *rWidget;
+    QWidget *hisWidget;
+    QWidget *noWidget;
+    QWidget *minimWidget;
+    QWidget *maximWidget;
+    QWidget *closeWidget;
+    QHBoxLayout *layout;
+    QHBoxLayout *mainLayout;
+    QHBoxLayout *txtLayout;
+    QHBoxLayout *titLayout;
+    QVBoxLayout *hisTxtLayout;
+    QVBoxLayout *hotLayout;
+    QVBoxLayout *hisLayout;
+    QVBoxLayout *hisListLayout;
+    QLineEdit *searInput;
+    QPixmap *avatorPix;
+    QNetworkAccessManager *manager;
+    QNetworkRequest *request;
+    QNetworkReply *reply;
+    QEventLoop *loop;
+    QByteArray *byteArray;
+    QPainter *painter;
+    QPainterPath *path;
+};
+
+
+#endif //QQMUSICCOPY_SETTMENU_H
