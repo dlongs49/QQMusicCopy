7bb102aa7823f6e6e1a30a72472b429e4fc020ce
dinglong <dillonl49@163.com>
2024-03-27 17:29:29 +0800

    fix:#修改设置弹框位置

--- src/lib/popup/settMenu.cpp
+++ src/lib/popup/settMenu.cpp
@@ -8,7 +8,7 @@ SettMenu::SettMenu(QWidget *parent) : QWidget(parent) {
     loadQSS();
 //    this->hide();
     int h = 596;
-    this->setGeometry(150, 44, w + 18, h + 18);
+    this->setGeometry(500, 44, w + 18, h + 18);
     // 边框阴影 + 圆角
     frame = new QFrame(this);
     frame->setGeometry(9, 9, w, h);
