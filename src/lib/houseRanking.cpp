1b8ef8d9c88a691bb7e705141c23e072eaecbe9f
dinglong <dillonl49@163.com>
2024-04-10 17:13:38 +0800

    feat:#增加排行页面

---
+++ src/lib/houseRanking.cpp
@@ -0,0 +1,216 @@
+//
+// Created by dinglong on 2024/4/10 17:04
+//
+
+#include "houseRanking.h"
+
+HouseRanking::HouseRanking(QWidget *parent) : QWidget(parent) {
+    installEventFilter(this);
+    loadQSS();
+    tools = new Tools();
+
+    this->setFixedSize(820, 2000);
+    widget = new QWidget(this);
+    widget->setObjectName("conbox");
+    layout = new QVBoxLayout;
+    layout->setAlignment(Qt::AlignTop | Qt::AlignHCenter);
+    layout->setSpacing(0);
+    layout->setMargin(0);
+
+    scrollArea = new QScrollArea(this);
+    scrollArea->setAlignment(Qt::AlignTop);
+    scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    scrollArea->setGeometry(0, 0, 820, 500);
+    scrollArea->setWidget(widget);
+    scrollArea->setWidgetResizable(true);
+
+    rankingTop();
+    regional();
+    feature();
+    globa();
+    widget->setLayout(layout);
+
+
+}
+
+void HouseRanking::rankingTop() {
+
+    containerBox = new QWidget(widget);
+    containerBox->setFixedSize(widget->width() - 20, 178);
+    containerBox->installEventFilter(this);
+    containerBox->setObjectName("selectedBox");
+
+    contentLayout = new QHBoxLayout;
+    contentLayout->setSpacing(0);
+    contentLayout->setMargin(0);
+    contentLayout->setAlignment(Qt::AlignTop);
+    containerBox->setLayout(contentLayout);
+
+    scrollBox = new QWidget(containerBox);
+    scrollBox->setFixedSize(containerBox->width() - 60, containerBox->height());
+    scrollBox->setObjectName("bannerBox");
+
+    bannerLayout = new QHBoxLayout;
+    bannerLayout->setSpacing(0);
+    bannerLayout->setMargin(0);
+    bannerLayout->setAlignment(Qt::AlignLeft);
+
+    bannerBox = new QWidget(scrollBox);
+    bannerBox->setLayout(bannerLayout);
+    animation[0] = new QPropertyAnimation(bannerBox, "geometry");
+    animation[0]->setDuration(300);
+
+    QList<QString> imgList;
+    QList<QString> txtList;
+    imgList << "https://y.qq.com/music/common/upload/MUSIC_FOCUS/6483999.jpg"
+            << "https://y.qq.com/music/common/upload/MUSIC_FOCUS/6484203.jpg"
+            << "https://y.qq.com/music/common/upload/ocs/2681676c054da54091a9f000208e81bf.png"
+            << "https://y.qq.com/music/common/upload/MUSIC_FOCUS/6482174.jpg";
+    for (int i = 0; i < imgList.size(); ++i) {
+        bannerItem[i] = new QWidget;
+        bannerItem[i]->installEventFilter(this);
+        bannerItem[i]->setObjectName("bannerItem");
+        bannerItem[i]->setFixedSize(240, containerBox->height());
+        bannerImgLayout = new QVBoxLayout;
+        bannerImgLayout->setSpacing(0);
+        bannerImgLayout->setMargin(0);
+        bannerImgLayout->setAlignment(Qt::AlignTop);
+
+        bannerImg = new QLabel(bannerItem[i]);
+        bannerImg->setCursor(Qt::PointingHandCursor);
+        bannerImg->setScaledContents(true);
+        bannerImg->setObjectName("bannerImg");
+        bannerImg->setProperty("index", i);
+        bannerImg->installEventFilter(this);
+        // 先提取网络图片 再处理圆角 Tools
+        bannerImg->setPixmap(tools->imgPixRadius(getImage(imgList[i]), getImage(imgList[i])->size(), 16));
+        bannerImg->setFixedSize(240, containerBox->height());
+
+        bomBox = new QWidget(bannerImg);
+        bomBox->setGeometry(0, bannerImg->height() - 40, bannerImg->width(), 40);
+        bomLayout = new QHBoxLayout;
+        bomLayout->setAlignment(Qt::AlignBottom);
+        bomBox->setLayout(bomLayout);
+
+        tagLabel = new QLabel;
+        tagLabel->setObjectName("tagLabel");
+        tagLabel->setText("音乐人");
+        tagLabel->setFixedSize(tagLabel->sizeHint().width() + 5, tagLabel->sizeHint().height());
+
+        titleLabel = new QLabel;
+        QString txt = tools->textElps("贺峻霖全新单曲《缘故》正式上线 ", 150, titleLabel->font());
+        titleLabel->setText(txt);
+        titleLabel->setObjectName("titleLabel");
+
+        bomLayout->addWidget(tagLabel);
+        bomLayout->addWidget(titleLabel);
+
+        bannerImgLayout->addWidget(bannerImg);
+        bannerItem[i]->setLayout(bannerImgLayout);
+        bannerLayout->addWidget(bannerItem[i]);
+        if (i != imgList.size() - 1) {
+            bannerLayout->addSpacing(20);
+        }
+    }
+    layout->addSpacing(10);
+    layout->addWidget(containerBox);
+    layout->addSpacing(30);
+}
+
+// 地区榜
+void HouseRanking::regional() {
+
+}
+
+// 特色榜
+void HouseRanking::feature() {
+}
+
+// 全球榜
+void HouseRanking::globa() {
+}
+
+QPixmap *HouseRanking::getImage(QString url) {
+    manager = new QNetworkAccessManager;
+    reply = manager->get(QNetworkRequest(QUrl(url)));
+    loop = new QEventLoop();
+    connect(reply, SIGNAL(finished()), loop, SLOT(quit()));
+    loop->exec();
+    QByteArray imgData = reply->readAll();
+    coverImg = new QPixmap;
+    coverImg->loadFromData(imgData);
+    return coverImg;
+}
+
+// 向上动画
+void HouseRanking::toggleItem(QWidget *itemBox, QString objName, QEvent *e) {
+    QLabel *box = itemBox->findChild<QLabel *>(objName);
+    QWidget *mask = itemBox->findChild<QWidget *>("maskBox");
+    QLabel *play_box = itemBox->findChild<QLabel *>("playBox");
+    QLabel *play_count = itemBox->findChild<QLabel *>("playCount");
+
+
+    QPropertyAnimation *animation = new QPropertyAnimation(box, "geometry");
+    animation->setDuration(150);
+
+    if (e->type() == QEvent::Enter) {
+        animation->setStartValue(QRect(0, 10, box->width(), box->height()));
+        animation->setEndValue(QRect(0, 0, box->width(), box->height()));
+        animation->start();
+        if (play_box) {
+            play_box->setVisible(true);
+        }
+        if (mask) {
+            mask->setVisible(true);
+        }
+        if (play_count) {
+            play_count->setVisible(false);
+        }
+    }
+    if (e->type() == QEvent::Leave) {
+        animation->setStartValue(QRect(0, 0, box->width(), box->height()));
+        animation->setEndValue(QRect(0, 10, box->width(), box->height()));
+        animation->start();
+        if (play_box) {
+            play_box->setVisible(false);
+        }
+        if (mask) {
+            mask->setVisible(false);
+        }
+        if (play_count) {
+            play_count->setVisible(true);
+        }
+    }
+}
+
+
+bool HouseRanking::eventFilter(QObject *o, QEvent *e) {
+    if (o->objectName() == "officialImg") {
+        int i = o->property("index").toInt();
+        toggleItem(officialItem[i], "officialImg", e);
+    }
+    if (o->objectName() == "listenBookImg") {
+        int i = o->property("index").toInt();
+        toggleItem(listenBookItem[i], "listenBookImg", e);
+    }
+
+    if (o->objectName() == "latestImg") {
+        int i = o->property("index").toInt();
+        toggleItem(latestItem[i], "latestImg", e);
+    }
+    if (o->objectName() == "classPrefeImg") {
+        int i = o->property("index").toInt();
+        toggleItem(classItem[i], "classPrefeImg", e);
+    }
+
+    return QWidget::eventFilter(o, e);
+
+}
+
+void HouseRanking::loadQSS() {
+    QFile qss(":/resource/qss/houseRanking.qss");
+    qss.open(QFile::ReadOnly);
+    QString style = QLatin1String(qss.readAll());
+    this->setStyleSheet(style);
+    qss.close();
+}
---
+++ src/lib/houseRanking.h
@@ -0,0 +1,101 @@
+//
+// Created by dinglong on 2024/4/10 17:04
+//
+
+#ifndef QQMUSICCOPY_HOUSERANKING_H
+#define QQMUSICCOPY_HOUSERANKING_H
+
+#include <QWidget>
+#include <QScrollArea>
+#include <QList>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QSizePolicy>
+#include <QPropertyAnimation>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QEventLoop>
+#include <QByteArray>
+#include <QMouseEvent>
+#include <cmath>
+#include "../utils/tools.h"
+
+class HouseRanking : public QWidget {
+Q_OBJECT;
+public:
+    HouseRanking(QWidget *parent = nullptr);
+
+
+signals:
+
+
+public slots:
+
+    bool eventFilter(QObject *watched, QEvent *event);
+
+    void loadQSS();
+
+    void rankingTop();
+    void regional();
+    void feature();
+    void globa();
+    void toggleItem(QWidget *itemBox, QString objName, QEvent *e);
+
+    QPixmap *getImage(QString url);
+
+protected:
+
+private:
+    int move_x = 0;
+    QList<QString> recomAttrList;
+    QPropertyAnimation *animation[6];
+    QScrollArea *scrollArea;
+    QLabel *tagLabel;
+    QLabel *titleLabel;
+    QLabel *title;
+    QLabel *subTitle;
+    QLabel *exTitle;
+    QLabel *playBox;
+    QLabel *bannerImg;
+    QLabel *playCount;
+    QLabel *moreTitle;
+    QLabel *arrowTitle;
+    QLabel *itemImg;
+    QLabel *rightMoreBox;
+    QWidget *widget;
+    QWidget *containerBox;
+    QWidget *scrollBox;
+    QWidget *contentBox;
+    QWidget *bannerBox;
+    QWidget *wrapconBox[6];
+    QLabel *latestNavItem[6];
+    QWidget *maskBox;
+    QWidget *latestNavBox;
+    QWidget *titleBox;
+    QWidget *leftTitBox;
+    QWidget *bomBox;
+    QWidget *bannerItem[10];
+    QWidget *officialItem[6];
+    QWidget *listenBookItem[6];
+    QWidget *classItem[6];
+    QWidget *latestItem[6];
+    QVBoxLayout *layout;
+    QVBoxLayout *containerLayout;
+    QVBoxLayout *bannerImgLayout;
+    QVBoxLayout *itemLayout;
+    QHBoxLayout *contentLayout;
+    QHBoxLayout *bannerLayout;
+    QHBoxLayout *bomLayout;
+    QHBoxLayout *titleLayout;
+    QHBoxLayout *titLeftLayout;
+    QHBoxLayout *moreRightLayout;
+    QHBoxLayout *latestNavLayout;
+    QNetworkAccessManager *manager;
+    QNetworkReply *reply;
+    QEventLoop *loop;
+    QPixmap *coverImg;
+    Tools *tools;
+};
+
+#endif //QQMUSICCOPY_HOUSERANKING_H
