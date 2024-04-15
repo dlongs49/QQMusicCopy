1285b694e1ce84dfa9377d7d40928321f92fb9a1
dinglong <dillonl49@163.com>
2024-04-15 15:04:55 +0800

    style:#取消歌手滚动区域

--- src/lib/houseSinger.cpp
+++ src/lib/houseSinger.cpp
@@ -10,20 +10,14 @@ HouseSinger::HouseSinger(QWidget *parent) : QWidget(parent) {
     tools = new Tools();
     // 更多qq音乐客户端对应的页面 https://y.qq.com/m/client/config/url.pc.json?pcachetime=1712844941
     //对应的qq页面  https://y.qq.com/wk_v17/#/musicroom/recommend
-    this->setFixedSize(820, 1000);
+    this->setFixedSize(820, 1400);
     widget = new QWidget(this);
+    widget->setFixedSize(this->size());
     widget->setObjectName("conbox");
     layout = new QVBoxLayout;
     layout->setAlignment(Qt::AlignTop | Qt::AlignHCenter);
     layout->setSpacing(0);
     layout->setMargin(0);
-
-    scrollArea = new QScrollArea(this);
-    scrollArea->setAlignment(Qt::AlignTop);
-    scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    scrollArea->setGeometry(0, 0, 820, 500);
-    scrollArea->setWidget(widget);
-    scrollArea->setWidgetResizable(true);
 
     singerTop();
     singerList();
