97b0d6dce09adfb15b62e1e8c6093832f5b3daff
dinglong <dillonl49@163.com>
2024-04-15 15:31:06 +0800

    style:#取消排行榜滚动区域

--- src/lib/houseRanking.h
+++ src/lib/houseRanking.h
@@ -6,12 +6,8 @@
 #define QQMUSICCOPY_HOUSERANKING_H
 
 #include <QWidget>
-#include <QScrollArea>
-#include <QList>
 #include <QLabel>
 #include <QHBoxLayout>
-#include <QScrollArea>
-#include <QSizePolicy>
 #include <QPropertyAnimation>
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
@@ -49,49 +45,28 @@ public slots:
 protected:
 
 private:
-    int move_x = 0;
-    QList<QString> recomAttrList;
-    QPropertyAnimation *animation[6];
-    QScrollArea *scrollArea;
-    QLabel *tagLabel;
-    QLabel *titleLabel;
     QLabel *title;
-    QLabel *subTitle;
-    QLabel *exTitle;
     QLabel *playBox;
-    QLabel *moreTitle;
     QLabel *itemImg;
     QLabel *playSicon;
     QLabel *playCount;
     QLabel *songItem[3];
     QWidget *widget;
     QWidget *containerBox;
-    QWidget *contentBox;
     QWidget *playCountBox;
-    QWidget *wrapconBox[6];
-    QLabel *latestNavItem[6];
     QWidget *maskBox;
     QWidget *containerVBox;
-    QWidget *titleBox;
-    QWidget *leftTitBox;
     QWidget *rightListBox;
     QWidget *item[10];
     QWidget *regItem[10];
     QWidget *feaItem[10];
     QWidget *globItem[10];
-    QWidget *listenBookItem[6];
-    QWidget *classItem[6];
-    QWidget *latestItem[6];
     QVBoxLayout *layout;
     QVBoxLayout *rightListLayout;
     QVBoxLayout *containerVLayout;
     QHBoxLayout *contentLayout;
-    QHBoxLayout *bannerLayout;
-    QHBoxLayout *bomLayout;
     QHBoxLayout *playCountLayout;
     QHBoxLayout *containerHLayout;
-    QHBoxLayout *moreRightLayout;
-    QHBoxLayout *latestNavLayout;
     QGridLayout *containerLayout;
     QNetworkAccessManager *manager;
     QNetworkReply *reply;
