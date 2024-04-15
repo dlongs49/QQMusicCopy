2aa9bfed4d56690a6f86ac2a4177184e52488da8
dinglong <dillonl49@163.com>
2024-04-15 15:26:34 +0800

    style:#取消歌手滚动区域

--- src/lib/houseSinger.h
+++ src/lib/houseSinger.h
@@ -6,13 +6,11 @@
 #define QQMUSICCOPY_HOUSESINGER_H
 
 #include <QWidget>
-#include <QScrollArea>
 #include <QList>
 #include <QLabel>
 #include <QHBoxLayout>
-#include <QScrollArea>
-#include <QSizePolicy>
-#include <QPropertyAnimation>
+#include <QVBoxLayout>
+#include <QGridLayout>
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 #include <QNetworkReply>
@@ -44,48 +42,15 @@ public slots:
 protected:
 
 private:
-    int move_x = 0;
-    QList<QString> recomAttrList;
-    QPropertyAnimation *animation[6];
-    QScrollArea *scrollArea;
-    QLabel *tagLabel;
-    QLabel *titleLabel;
-    QLabel *title;
-    QLabel *subTitle;
-    QLabel *exTitle;
-    QLabel *playBox;
     QLabel *singerName;
     QLabel *itemImg;
-    QLabel *playSicon;
-    QLabel *playCount;
-    QLabel *songItem[3];
     QWidget *widget;
     QWidget *containerBox;
-    QWidget *contentBox;
-    QWidget *playCountBox;
-    QWidget *wrapconBox[6];
-    QLabel *latestNavItem[6];
-    QWidget *maskBox;
-    QWidget *containerVBox;
-    QWidget *titleBox;
-    QWidget *leftTitBox;
-    QWidget *rightListBox;
     QWidget *singerItem[10];
-    QWidget *item[40];
-    QWidget *feaItem[10];
-    QWidget *globItem[10];
-    QWidget *listenBookItem[6];
-    QWidget *classItem[6];
-    QWidget *latestItem[6];
+    QWidget *item[55];
     QVBoxLayout *layout;
     QVBoxLayout *contentLayout;
     QHBoxLayout *itemLayout;
-    QHBoxLayout *bannerLayout;
-    QHBoxLayout *bomLayout;
-    QHBoxLayout *playCountLayout;
-    QHBoxLayout *containerHLayout;
-    QHBoxLayout *moreRightLayout;
-    QHBoxLayout *latestNavLayout;
     QGridLayout *containerLayout;
     QNetworkAccessManager *manager;
     QNetworkReply *reply;
